7a071d878df548d6d9f0c7d230a3793fcd7cb1f5
KYO97 <htd19972012@gmail.com>
2021-04-23 21:38:59 +0700

    NEW

---
+++ pedestrian_detection/build/ncnn_build/examples/CMakeFiles/squeezenet_c_api.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/nghiep/Desktop/test/current1/person_detection/ncnn/examples/squeezenet_c_api.cpp" "/home/nghiep/Desktop/test/current1/person_detection/pedestrian_detection/build/ncnn_build/examples/CMakeFiles/squeezenet_c_api.dir/squeezenet_c_api.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/nghiep/Desktop/test/current1/person_detection/ncnn/examples/../src"
+  "ncnn_build/examples/../src"
+  "/usr/local/include/opencv4"
+  "/home/nghiep/Desktop/test/current1/person_detection/ncnn/src"
+  "ncnn_build/src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/nghiep/Desktop/test/current1/person_detection/pedestrian_detection/build/ncnn_build/src/CMakeFiles/ncnn.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- pedestrian_detection/test.cpp
+++ pedestrian_detection/test.cpp
@@ -5,26 +5,32 @@
 #include <chrono>
 #include <iostream>
 
+using namespace std;
+
 int main( int argc, char** argv )
 {
-    cv::VideoCapture cap(-1);
+    std::string pipeline = "v4l2src device=/dev/video1 ! image/jpeg, width=(int)1280, height=(int)720, framerate=30/1 ! jpegdec ! videoconvert ! appsink";
+    cv::VideoCapture cap(pipeline, cv::CAP_GSTREAMER);
     cv::Mat frame;
 
     ObjectDetection det("../../pedestrian_detection/");
-
+    auto m_StartTime = std::chrono::system_clock::now();
     while(true)
     {
         cap >> frame;
-        std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
-        auto recs = det.detectObject(frame);
-        std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
-        std::chrono::duration<double> time_used = std::chrono::duration_cast<std::chrono::duration<double>>(t2-t1);
-        std::cout<<"Optical flow time: " << time_used.count() << "seconds" << std::endl;
+	double fps = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - m_StartTime).count();
+	m_StartTime = std::chrono::system_clock::now();
+	cv::putText(frame, to_string(static_cast<int>(1000/fps)) + " FPS", cv::Point(10, 30), cv::FONT_HERSHEY_DUPLEX, 1, cv::Scalar(0, 0, 255), 1, false);
+        //std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
+        //auto recs = det.detectObject(frame);
+        //std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
+        //std::chrono::duration<double> time_used = std::chrono::duration_cast<std::chrono::duration<double>>(t2-t1);
+        //std::cout<<"Optical flow time: " << time_used.count() << "seconds" << std::endl;
 
-        for(auto rec:recs)
-        {
-            cv::rectangle(frame, rec, cv::Scalar(0, 255, 0), 2, 1);
-        }
+        //for(auto rec:recs)
+        //{
+        //    cv::rectangle(frame, rec, cv::Scalar(0, 255, 0), 2, 1);
+        //}
 
         cv::imshow("detection", frame);
         if(cv::waitKey(1) == 27)
@@ -32,4 +38,7 @@ int main( int argc, char** argv )
             break;
         }
     }
+    cap.release();
+    cv::destroyAllWindows() ;
+    return 0;
 }
